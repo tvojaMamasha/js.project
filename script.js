0f0c29a8e6aacc10cf82cee552717246ddf9581e
TvojaMamasha <deh1997a03@gmail.com>
2021-11-30 22:47:31 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const numberOfFilm = +prompt('сколько фильмов вы уже просмотрели?', '');
+
+const personalMovieDB = {
+    count: personalMovieDB,
+    movies: {},
+    actors {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотрених фильмов?', ''),
+      b = prompt('на сколько оцените его?', ''),
+      c = prompt('Один из последних просмотрених фильмов?', ''),
+      d = prompt('на сколько оцените его?', '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
