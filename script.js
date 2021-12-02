9c16bd85dc35de19dd26182fcae80c8fd02f19b4
TvojaMamasha <deh1997a03@gmail.com>
2021-12-02 10:34:35 +0200

    second commit

--- script.js
+++ script.js
@@ -17,3 +17,43 @@ const a = prompt('Один из последних просмотрених фи
       personalMovieDB.movies[c] = d;
 
       console.log(personalMovieDB);
+
+      "use strict"
+
+      if (1) {
+          console.log('OK!');
+          else {
+              console.log('error');
+          }
+      }
+
+      const num = 50;
+
+        if (num < 49) {
+            console.log('Error');
+        }   else if (num > 100) {
+            console.log('Mnogo');
+        }   else {
+            console.log('Ok!')
+        }
+
+        const num = 50;
+
+        (num === 50) ? console.log('Ok!') : console.log('Error');
+
+
+       let num = 50;
+        // while (num <= 55) {
+        //    console.log(num);
+        //     num++;
+        // }
+
+        do {
+            console.log(num);
+            num++;
+        }
+        while (num < 55);
+
+        for (let i = 1; i < 8; i++) {
+            console.log(i)
+        }
